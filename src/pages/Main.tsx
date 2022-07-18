2898eeb57e91531112e2a62f988462d138d0e1da
Jinwoo Jeong <thinkratomos@gmail.com>
2022-07-19 00:05:49 +0900

    feat: scroll to top when route

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,12 +3,23 @@ import { StatusBar } from "expo-status-bar";
 import { NamuWiki } from "../components/NamuWiki";
 import { Result } from "../components/Result";
 import { SearchBar } from "../components/SearchBar";
+import { useEffect, useRef } from "react";
+import { useIsLoading } from "../hooks/useSearch";
 
 export default function Main() {
+  const scrollRef = useRef<ScrollView | null>(null);
+  const isLoading = useIsLoading();
+
+  useEffect(() => {
+    if (!isLoading) {
+      scrollRef.current?.scrollTo({ x: 0 });
+    }
+  }, [isLoading]);
+
   return (
     <View>
       <StatusBar style="auto" />
-      <ScrollView style={{ minHeight: "100%" }}>
+      <ScrollView style={{ minHeight: "100%" }} ref={scrollRef}>
         <View>
           <Result />
         </View>
